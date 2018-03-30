878df434acdc44ba35572e13bc705450b9e504dd
Erika <erikaek@student.chalmers.se>
2018-03-30 10:31:13 +0200

    fix

--- unet3d/training.py
+++ unet3d/training.py
@@ -22,7 +22,7 @@ def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0
     callbacks = list()
     callbacks.append(ModelCheckpoint(model_file, save_best_only=True))
     callbacks.append(CSVLogger(logging_file, append=True))
-    keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=6, write_graph=False, write_grads=False,
+    callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=6, write_graph=False, write_grads=False,
      write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
 
     if learning_rate_epochs:
