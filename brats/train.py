5468ac701b28ee0c6a981cc94ebc60219174f1a4
Erika <erikaek@student.chalmers.se>
2018-04-01 10:52:45 +0200

    smaller patch size

--- brats/train.py
+++ brats/train.py
@@ -10,7 +10,7 @@ from unet3d.training import load_old_model, train_model
 config = dict()
 config["pool_size"] = (2, 2, 2)  # pool size for the max pooling operations
 config["image_shape"] = (144, 144, 144)  # This determines what shape the images will be cropped/resampled to.
-config["patch_shape"] = (64, 64, 64)  # switch to None to train on the whole image
+config["patch_shape"] = (32, 32, 32)  # switch to None to train on the whole image
 config["labels"] = (1, 2, 4)  # the label numbers on the input image
 config["n_labels"] = len(config["labels"])
 config["all_modalities"] = ["t1", "t1Gd", "flair", "t2"]
