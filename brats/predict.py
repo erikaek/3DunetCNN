19a0b77d2e1b11573d630458d1a045619efb0690
Erika <erikaek@student.chalmers.se>
2018-04-03 14:20:33 +0200

    added model mode option

--- brats/predict.py
+++ brats/predict.py
@@ -6,40 +6,44 @@ from train_isensee2017 import config as config_isensee
 from unet3d.prediction import run_validation_cases
 
 
-def main():
+def main(args):
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument("mode", help="enter model mode: unet OR isensee")
-	args = parser.parse_args()
-	
-	if args.mode.lower() == 'unet':
+	prediction_dir = os.path.abspath("./prediction/"+args.mode.lower())
 
-    	prediction_dir = os.path.abspath("./unet/prediction")
-    	run_validation_cases(validation_keys_file=config_unet["validation_file"],
+	if not os.path.exists(prediction_dir):
+
+		os.makedirs(prediction_dir)
+
+	if args.mode.lower() == "unet":
+
+		run_validation_cases(validation_keys_file=config_unet["validation_file"],
         	                 model_file=config_unet["model_file"],
             	             training_modalities=config_unet["training_modalities"],
                 	         labels=config_unet["labels"],
                     	     hdf5_file=config_unet["data_file"],
                     	  	 output_label_map=True,
                          	 output_dir=prediction_dir)
+	elif args.mode.lower() == "isensee2017":
 
-    elif args.mode.lower() == 'isensee':
-
-    	prediction_dir = os.path.abspath("./isensee2017/prediction")
-    	run_validation_cases(validation_keys_file=config_isensee["validation_file"],
+		run_validation_cases(validation_keys_file=config_isensee["validation_file"],
         	                 model_file=config_isensee["model_file"],
             	             training_modalities=config_isensee["training_modalities"],
                 	         labels=config_isensee["labels"],
                     	     hdf5_file=config_isensee["data_file"],
                     	  	 output_label_map=True,
                          	 output_dir=prediction_dir)
+	else:
 
-    else : 
-
-    	raise Exception('Unvalid model mode! Enter unet OR isensee')
+		raise Exception("ERROR: Unvalid model mode! Enter unet OR isensee")
 
 
 
 
 if __name__ == "__main__":
-    main()
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("mode", help="enter model mode: unet OR isensee2017")
+	args = parser.parse_args()
+
+	main(args)
+    	
