f3dd0a927c1b95b9a61bdc0501d2a06aa63df864
Erika <erikaek@student.chalmers.se>
2018-04-04 09:37:18 +0200

    changes

--- headneck/conversion.py
+++ headneck/conversion.py
@@ -1,6 +1,7 @@
 
 
 import vtk
+import nrrd
 import os.path
 
 def readnrrd(filename):
@@ -20,8 +21,8 @@ def writenifti(image,filename, info):
     writer.Write()
 
 def definefolders():
-    organs = {'BrainStem', 'Chiasm', 'Mandible', 'OpticNerve_L', 'OpticNerve_R','Parotid_L',
-              'Parotid_R', 'Submandibular_L', 'Submandibular_R'};
+    organs = ['BrainStem', 'Chiasm', 'Mandible', 'OpticNerve_L', 'OpticNerve_R','Parotid_L',
+              'Parotid_R', 'Submandibular_L', 'Submandibular_R'];
 
     part1_part2_fold = ['0522c0001', '0522c0002', '0522c0003', '0522c0009', '0522c0013',
     '0522c0014', '0522c0017', '0522c0057', '0522c0070', '0522c0077', '0522c0079',
@@ -37,21 +38,47 @@ def definefolders():
 
     part_folders = ['PDDCA-1.4.1_part1_part2', 'PDDCA-1.4.1_part3', 'PDDCA-1.4.1_part4']
 
-    data_folders = [part1_part2_fold, part3_fold, part4_fold];
+    data_folders = {0 : part1_part2_fold, 1 : part3_fold, 2 : part4_fold};
 
     return organs, part_folders, data_folders
 
 organs, part_folders, data_folders = definefolders()
 
-for df in data_folders[:2]:
+for i_pf in range(2):
 
-    for pf in part_folders:
-        readpath = '../../Data/'+df+'/'+pf+'/'
-        savepath = 'data/original/'+pf+'/'
+    pf = part_folders[i_pf]
 
-        if os.path.exists(readpath+'structures/'+organs[3]+'.nrrd'):
-            label_data, info = readnrrd(readpath+'structures/'+organs[3]+'.nrrd')
-            writenifti(label_data, savepath + 'img.nii', info)
-            ct_data, info = readnrrd(datapath+'img.nrrd')
-            writenifti(ct_data, savepath+'img.nii', info)
+    for df in data_folders[i_pf]:
+        readpath = '../../Data/'+pf+'/'+df+'/'
+        savepath = 'data/original/'+df+'/'
+
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+
+        if os.path.exists(readpath+'structures/'+organs[2]+'.nrrd'):
+            label_data, info = nrrd.read(readpath+'structures/'+organs[2]+'.nrrd')
+            writenifti(label_data, savepath + 'labels.nii.gz', info)
+            ct_data, info = nrrd.read(readpath+'img.nrrd')
+            writenifti(ct_data, savepath+'img.nii.gz', info)
+
+
+
+
+"""for i_pf in range(2):
+
+    pf = part_folders[i_pf]
+
+    for df in data_folders[i_pf]:
+        readpath = '../../Data/'+pf+'/'+df+'/'
+        savepath = 'data/original/'+df+'/'
+
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+
+        if os.path.exists(readpath+'structures/'+organs[2]+'.nrrd'):
+            label_data, info = readnrrd(readpath+'structures/'+organs[2]+'.nrrd')
+            writenifti(label_data, savepath + 'labels.nii.gz', info)
+            ct_data, info = readnrrd(readpath+'img.nrrd')
+            writenifti(ct_data, savepath+'img.nii.gz', info)
+"""
 
