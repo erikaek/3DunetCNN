2ad8d5c44d95403644824a063fe004b25ccac85e
Erika <erikaek@student.chalmers.se>
2018-04-10 09:51:31 +0200

    added headneck prediction and evaluation

---
+++ headneck/evaluate.py
@@ -0,0 +1,66 @@
+import numpy as np
+import nibabel as nib
+import os
+import argparse
+import glob
+import pandas as pd
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+
+def get_background_mask(data):
+    return data == 0
+
+
+def get_mandible_mask(data):
+    return data == 1
+
+
+def dice_coefficient(truth, prediction):
+    return 2 * np.sum(truth * prediction)/(np.sum(truth) + np.sum(prediction))
+
+
+def main(args):
+    header = ("Background", "Mandible")
+    masking_functions = (get_background_mask, get_mandible_mask)
+    rows = list()
+    prediction_path = "./prediction/"+args.mode.lower()+"/"
+    for case_folder in glob.glob(prediction_path+"*/"):
+        truth_file = os.path.join(case_folder, "truth.nii.gz")
+        truth_image = nib.load(truth_file)
+        truth = truth_image.get_data()
+        prediction_file = os.path.join(case_folder, "prediction.nii.gz")
+        prediction_image = nib.load(prediction_file)
+        prediction = prediction_image.get_data()
+        rows.append([dice_coefficient(func(truth), func(prediction))for func in masking_functions])
+    df = pd.DataFrame.from_records(rows, columns=header)
+    df.to_csv(prediction_path+"headneck_scores.csv")
+
+    scores = dict()
+    for index, score in enumerate(df.columns):
+        values = df.values.T[index]
+        scores[score] = values[np.isnan(values) == False]
+
+    plt.boxplot(list(scores.values()), labels=list(scores.keys()))
+    plt.ylabel("Dice Coefficient")
+    plt.savefig(prediction_path+"validation_scores_boxplot.png")
+    plt.close()
+
+    training_df = pd.read_csv("./headneck/isensee2017/training.log").set_index('epoch')
+
+    plt.plot(training_df['loss'].values, label='training loss')
+    plt.plot(training_df['val_loss'].values, label='validation loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.xlim((0, len(training_df.index)))
+    plt.legend(loc='upper right')
+    plt.savefig(prediction_path+'loss_graph.png')
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("mode", help="enter model mode: unet OR isensee2017")
+	args = parser.parse_args()
+
+	main(args)
---
+++ headneck/predict.py
@@ -0,0 +1,49 @@
+import os
+import argparse
+
+from train import config as config_unet
+from train_isensee2017 import config as config_isensee
+from unet3d.prediction import run_validation_cases
+
+
+def main(args):
+
+	prediction_dir = os.path.abspath("./prediction/"+args.mode.lower())
+
+	if not os.path.exists(prediction_dir):
+
+		os.makedirs(prediction_dir)
+
+	if args.mode.lower() == "unet":
+
+		run_validation_cases(validation_keys_file=config_unet["validation_file"],
+        	                 model_file=config_unet["model_file"],
+            	             training_modalities=config_unet["training_modalities"],
+                	         labels=config_unet["labels"],
+                    	     hdf5_file=config_unet["data_file"],
+                    	  	 output_label_map=True,
+                         	 output_dir=prediction_dir)
+	elif args.mode.lower() == "isensee2017":
+
+		run_validation_cases(validation_keys_file=config_isensee["validation_file"],
+        	                 model_file=config_isensee["model_file"],
+            	             training_modalities=config_isensee["training_modalities"],
+                	         labels=config_isensee["labels"],
+                    	     hdf5_file=config_isensee["data_file"],
+                    	  	 output_label_map=True,
+                         	 output_dir=prediction_dir)
+	else:
+
+		raise Exception("ERROR: Unvalid model mode! Enter unet OR isensee")
+
+
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("mode", help="enter model mode: unet OR isensee2017")
+	args = parser.parse_args()
+
+	main(args)
+    	
