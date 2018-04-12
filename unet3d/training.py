29c69f74fde6cd9002953ac32901b83722c51edf
Erika <erikaek@student.chalmers.se>
2018-04-12 13:29:34 +0200

    added sample weight possibility

--- unet3d/training.py
+++ unet3d/training.py
@@ -59,7 +59,7 @@ def load_old_model(model_file):
 
 def train_model(model, model_file, training_generator, validation_generator, steps_per_epoch, validation_steps,
                 initial_learning_rate=0.001, learning_rate_drop=0.5, learning_rate_epochs=None, n_epochs=500,
-                learning_rate_patience=20, early_stopping_patience=None, logging_path="./brats/unet/"):
+                learning_rate_patience=20, early_stopping_patience=None, logging_path="./brats/unet/",sample_weight=None):
     """
     Train a Keras model.
     :param early_stopping_patience: If set, training will end early if the validation loss does not improve after the
@@ -85,6 +85,7 @@ def train_model(model, model_file, training_generator, validation_generator, ste
                         validation_steps=validation_steps,
                         max_queue_size=1,
                         use_multiprocessing=True,
+                        sample_weight=sample_weight
                         callbacks=get_callbacks(model_file,
                                                 initial_learning_rate=initial_learning_rate,
                                                 learning_rate_drop=learning_rate_drop,
