eaaf973eba178a6a8c5d6febc8b02b784a9aaa5e
Erika <erikaek@student.chalmers.se>
2018-04-16 11:30:02 +0200

    debugging

--- unet3d/generator.py
+++ unet3d/generator.py
@@ -217,6 +217,7 @@ def create_patch_index_list(index_list, image_shape, patch_shape, patch_overlap,
 
 def generate_sample_weight_matrix(sample_weight_vector, labels, shape):
     n_labels = len(labels)
+    labels = np.array(labels)
     sample_weight_matrix = np.zeros([shape[0],shape[1],shape[2],n_labels])
     for i_label in range(n_labels):
         sample_weight_vector[:,:,:,:,:,i_label] = labels[i_label]
