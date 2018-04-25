373ec1e062b3ae85f5bca911e2faf6e689f7682a
Erika <erikaek@student.chalmers.se>
2018-04-25 14:47:20 +0200

    debugging

--- unet3d/training.py
+++ unet3d/training.py
@@ -10,6 +10,105 @@ from unet3d.metrics import (dice_coefficient, dice_coefficient_loss, dice_coef, 
 
 K.set_image_dim_ordering('th')
 
+'''
+class ModelCheckpoint(Callback):
+    """Save the model after every epoch.
+    `filepath` can contain named formatting options,
+    which will be filled the value of `epoch` and
+    keys in `logs` (passed in `on_epoch_end`).
+    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
+    then the model checkpoints will be saved with the epoch number and
+    the validation loss in the filename.
+    # Arguments
+        filepath: string, path to save the model file.
+        monitor: quantity to monitor.
+        verbose: verbosity mode, 0 or 1.
+        save_best_only: if `save_best_only=True`,
+            the latest best model according to
+            the quantity monitored will not be overwritten.
+        mode: one of {auto, min, max}.
+            If `save_best_only=True`, the decision
+            to overwrite the current save file is made
+            based on either the maximization or the
+            minimization of the monitored quantity. For `val_acc`,
+            this should be `max`, for `val_loss` this should
+            be `min`, etc. In `auto` mode, the direction is
+            automatically inferred from the name of the monitored quantity.
+        save_weights_only: if True, then only the model's weights will be
+            saved (`model.save_weights(filepath)`), else the full model
+            is saved (`model.save(filepath)`).
+        period: Interval (number of epochs) between checkpoints.
+    """
+
+    def __init__(self, model, filepath, monitor='val_loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1):
+        super(ModelCheckpoint, self).__init__()
+        self.monitor = monitor
+        self.verbose = verbose
+        self.filepath = filepath
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.period = period
+        self.epochs_since_last_save = 0
+
+        self.model
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('ModelCheckpoint mode %s is unknown, '
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        self.epochs_since_last_save += 1
+        if self.epochs_since_last_save >= self.period:
+            self.epochs_since_last_save = 0
+            filepath = self.filepath.format(epoch=epoch + 1, **logs)
+            if self.save_best_only:
+                current = logs.get(self.monitor)
+                if current is None:
+                    warnings.warn('Can save best model only with %s available, '
+                                  'skipping.' % (self.monitor), RuntimeWarning)
+                else:
+                    if self.monitor_op(current, self.best):
+                        if self.verbose > 0:
+                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (epoch + 1, self.monitor, self.best,
+                                     current, filepath))
+                        self.best = current
+                        if self.save_weights_only:
+                            model.save_weights(filepath, overwrite=True)
+                        else:
+                            model.save(filepath, overwrite=True)
+                    else:
+                        if self.verbose > 0:
+                            print('\nEpoch %05d: %s did not improve from %0.5f' %
+                                  (epoch + 1, self.monitor, self.best))
+            else:
+                if self.verbose > 0:
+                    print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
+                if self.save_weights_only:
+                    model.save_weights(filepath, overwrite=True)
+                else:
+                    model.save(filepath, overwrite=True)
+'''
 
 # learning rate schedule
 def step_decay(epoch, initial_lrate, drop, epochs_drop):
@@ -20,16 +119,10 @@ def get_callbacks(model, model_file, initial_learning_rate=0.0001, learning_rate
                   learning_rate_patience=50, logging_path="./", verbosity=1,
                   early_stopping_patience=None):
     callbacks = list()
-
-    cbk = ModelCheckpoint(model_file, save_best_only=True)
-
-    if cbk:
-        cbk = cbk.set_model(model)
-
-    callbacks.append(cbk)
+    callbacks.append(ModelCheckpoint(model_file, save_best_only=True, save_weights_only=True))
     callbacks.append(CSVLogger(logging_path+"/training.log", append=True))
-    callbacks.append(TensorBoard(log_dir=logging_path+"/logs", histogram_freq=0, batch_size=2, write_graph=False, write_grads=False,
-     write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None))
+    #callbacks.append(TensorBoard(log_dir=logging_path+"/logs", histogram_freq=0, batch_size=2, write_graph=False, write_grads=False,
+    # write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None))
 
     if learning_rate_epochs:
         callbacks.append(LearningRateScheduler(partial(step_decay, initial_lrate=initial_learning_rate,
