22788d4fac265b5dca2baaab04c38a4093a55914
Erika <erikaek@student.chalmers.se>
2018-04-25 14:00:46 +0200

    debugging

--- unet3d/model/isensee2017.py
+++ unet3d/model/isensee2017.py
@@ -80,7 +80,7 @@ def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5
     parallel_model = multi_gpu_model(model, gpus=n_gpus)
     parallel_model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
 
-    #model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
+    model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
 
     return model, parallel_model
 
