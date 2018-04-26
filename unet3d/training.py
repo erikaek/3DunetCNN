100e62bbb3c360f63167fca33f1aa0dde899f825
Erika <erikaek@student.chalmers.se>
2018-04-26 18:46:22 +0200

    distortion, batch 2, change in model training

--- unet3d/training.py
+++ unet3d/training.py
@@ -19,7 +19,7 @@ def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0
                   learning_rate_patience=50, logging_path="./", verbosity=1,
                   early_stopping_patience=None):
     callbacks = list()
-    callbacks.append(ModelCheckpoint(model_file, save_best_only=True))
+    callbacks.append(ModelCheckpoint(model_file, monitor='val_loss', save_best_only=True, mode='min'))
     callbacks.append(CSVLogger(logging_path+"/training.log", append=True))
     #callbacks.append(TensorBoard(log_dir=logging_path+"/logs", histogram_freq=0, batch_size=2, write_graph=False, write_grads=False,
     # write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None))
@@ -28,10 +28,10 @@ def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0
         callbacks.append(LearningRateScheduler(partial(step_decay, initial_lrate=initial_learning_rate,
                                                        drop=learning_rate_drop, epochs_drop=learning_rate_epochs)))
     else:
-        callbacks.append(ReduceLROnPlateau(factor=learning_rate_drop, patience=learning_rate_patience,
+        callbacks.append(ReduceLROnPlateau(monitor='val_loss',factor=learning_rate_drop, patience=learning_rate_patience,
                                            verbose=verbosity))
     if early_stopping_patience:
-        callbacks.append(EarlyStopping(verbose=verbosity, patience=early_stopping_patience))
+        callbacks.append(EarlyStopping(monitor='val_loss', verbose=verbosity, patience=early_stopping_patience))
     return callbacks
 
 
