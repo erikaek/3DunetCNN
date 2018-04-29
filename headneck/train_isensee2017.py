bdb882bfbb913402111ea777eeda036738f95dba
Erika <erikaek@student.chalmers.se>
2018-04-29 09:51:21 +0200

    full img

--- headneck/train_isensee2017.py
+++ headneck/train_isensee2017.py
@@ -10,8 +10,8 @@ from unet3d.training import load_old_model, train_model
 
 
 config = dict()
-config["image_shape"] = (256, 256, 180)  # This determines what shape the images will be cropped/resampled to.
-config["patch_shape"] = (128, 128, 128)  # switch to None to train on the whole image
+config["image_shape"] = (128, 128, 128) #(256, 256, 180)  # This determines what shape the images will be cropped/resampled to.
+config["patch_shape"] = None #(128, 128, 128)  # switch to None to train on the whole image
 config["labels"] = (0,1)  # the label numbers on the input image
 config["n_base_filters"] = 16
 config["n_labels"] = len(config["labels"])
@@ -43,19 +43,19 @@ config["validation_patch_overlap"] = 0  # if > 0, during training, validation pa
 config["training_patch_start_offset"] = (16, 16, 16)  # randomly offset the first patch index by up to this offset
 config["skip_blank"] = True  # if True, then patches without any target will be skipped
 
-config["data_file"] = os.path.abspath("./headneck/isensee2017_gpu1/headneck_data.h5")
-config["model_file"] = os.path.abspath("./headneck/isensee2017_gpu1/isensee_2017_model.h5")
-config["training_file"] = os.path.abspath("./headneck/isensee2017_gpu1/isensee_training_ids.pkl")
-config["validation_file"] = os.path.abspath("./headneck/isensee2017_gpu1/isensee_validation_ids.pkl")
+config["data_file"] = os.path.abspath("./headneck/isensee2017_gpu0/headneck_data.h5")
+config["model_file"] = os.path.abspath("./headneck/isensee2017_gpu0/isensee_2017_model.h5")
+config["training_file"] = os.path.abspath("./headneck/isensee2017_gpu0/isensee_training_ids.pkl")
+config["validation_file"] = os.path.abspath("./headneck/isensee2017_gpu0/isensee_validation_ids.pkl")
 config["overwrite"] = False  # If True, will previous files. If False, will use previously written files.
-config["logging_path"] = os.path.abspath("./headneck/isensee2017_gpu1/training.log")
+config["logging_path"] = os.path.abspath("./headneck/isensee2017_gpu0/training.log")
 config["n_gpus"] = 1 # enter how many gpus you want to use
 
 
 def fetch_training_data_files(return_subject_ids=False):
     training_data_files = list()
     subject_ids = list()
-    for subject_dir in glob.glob(os.path.join(os.path.dirname(__file__), "data", "preprocessed_downsampled", "*", "*")):
+    for subject_dir in glob.glob(os.path.join(os.path.dirname(__file__), "data", "preprocessed_full", "*", "*")):
         subject_ids.append(os.path.basename(subject_dir))
         subject_files = list()
         for modality in config["training_modalities"] + ["truth"]:
