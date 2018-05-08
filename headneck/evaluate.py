a11eee1fde711978850340d35ce86dc49c408ad6
Erika <erikaek@student.chalmers.se>
2018-05-08 15:05:32 +0200

    debugging

--- headneck/evaluate.py
+++ headneck/evaluate.py
@@ -29,7 +29,7 @@ def get_organ_mask(data):
 def dice_coefficient(y_true, y_pred, smooth=0.00001):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
-    #intersection = K.sum(y_true_f * y_pred_f)
+    intersection = K.sum(y_true_f * y_pred_f)
     return 1 #(2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 #def dice_coefficient(truth, prediction):
