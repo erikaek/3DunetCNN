be15c445501260e3ed8e88918f277c55d8e78e6d
Erika <erikaek@student.chalmers.se>
2018-05-16 08:16:22 +0200

    mandible patches gpu1

--- headneck/train_isensee2017.py
+++ headneck/train_isensee2017.py
@@ -11,7 +11,7 @@ from unet3d.training import load_old_model, train_model
 
 config = dict()
 config["image_shape"] = (256, 256, 180)  # This determines what shape the images will be cropped/resampled to.
-config["patch_shape"] = None #(128, 128, 128)  # switch to None to train on the whole image
+config["patch_shape"] = (128, 128, 128)  # switch to None to train on the whole image
 config["labels"] = (0,1)  # the label numbers on the input image
 config["n_base_filters"] = 16
 config["n_labels"] = len(config["labels"])
