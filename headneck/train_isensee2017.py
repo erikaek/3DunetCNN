7f7b5281ca0986fab222c8b9d89c8bf7e751074e
Erika <erikaek@student.chalmers.se>
2018-05-24 11:20:12 +0200

    andible original gpu1

--- headneck/train_isensee2017.py
+++ headneck/train_isensee2017.py
@@ -10,8 +10,8 @@ from unet3d.training import load_old_model, train_model
 
 
 config = dict()
-config["image_shape"] = (256, 256, 180)  # This determines what shape the images will be cropped/resampled to.
-config["patch_shape"] = (128, 128, 128)  # switch to None to train on the whole image
+config["image_shape"] = (128, 128, 128)  # This determines what shape the images will be cropped/resampled to.
+config["patch_shape"] = None #(128, 128, 128)  # switch to None to train on the whole image
 config["labels"] = (0,1)  # the label numbers on the input image
 config["n_base_filters"] = 16
 config["n_labels"] = len(config["labels"])
